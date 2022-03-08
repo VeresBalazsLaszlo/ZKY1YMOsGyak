22f5cc20e295d0efc6c22c9eb16b7ab6c935367b
VBalazsLaszlo <redleslie50@gmail.com>
2022-03-08 18:34:41 +0100

    0308

---
+++ ZKY1YM_0308/zky1ym1fel.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	if(!system("dir")){
+	for(int i=0;i<1;i++)
+		{
+		printf("korte\n");
+		}
+	}
+	system("color a");
+	system("Nincs ilyen parancs");
+    return 0;
+}
---
+++ ZKY1YM_0308/zky1ym2fel.c
@@ -0,0 +1,9 @@
+int main()
+{
+    char command[20];
+    do{
+        scanf("%s", command);
+        system(command);
+    }while(true);
+    return 0;
+}
